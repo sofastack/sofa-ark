be608a0f3c2bed324f4b167730f0cd69dbb80a9d
QilongZhang <qilong.zql@alibaba-inc.com>
2018-03-21 13:31:14 +0800

    check of format is done.

--- sofa-ark-parent/core-impl/container/src/main/java/com/alipay/sofa/ark/container/service/classloader/ClassloaderServiceImpl.java
+++ sofa-ark-parent/core-impl/container/src/main/java/com/alipay/sofa/ark/container/service/classloader/ClassloaderServiceImpl.java
@@ -50,8 +50,7 @@ public class ClassloaderServiceImpl implements ClassloaderService {
     private static final ArkLogger                 LOGGER                         = ArkLoggerFactory
                                                                                       .getDefaultLogger();
 
-    private static final String
-            JAVA_AGENT_MARK                = "-javaagent:";
+    private static final String                    JAVA_AGENT_MARK                = "-javaagent:";
 
     private static final String                    JAVA_AGENT_OPTION_MARK         = "=";
 
