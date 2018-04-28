51e48af61ba9faf0f1f06a5164f7e67007d5a719
QilongZhang <qilong.zql@alibaba-inc.com>
2018-04-29 01:49:27 +0800

    adjust test case

--- sofa-ark-parent/support/ark-support-starter/src/test/java/com/alipay/sofa/ark/support/runner/CommonJUnit4Test.java
+++ sofa-ark-parent/support/ark-support-starter/src/test/java/com/alipay/sofa/ark/support/runner/CommonJUnit4Test.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.alipay.sofa.ark.support;
+package com.alipay.sofa.ark.support.runner;
 
 import org.junit.Assert;
 import org.junit.Test;
