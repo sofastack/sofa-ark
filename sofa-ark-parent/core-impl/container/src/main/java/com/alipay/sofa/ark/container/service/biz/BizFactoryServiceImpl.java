d0d5923ceaa77656bc85df1b47bdcb52c9e09857
QilongZhang <qilong.zql@alibaba-inc.com>
2018-07-12 08:34:52 +0800

    upgrade jmockit version

--- sofa-ark-parent/core-impl/container/src/main/java/com/alipay/sofa/ark/container/service/biz/BizFactoryServiceImpl.java
+++ sofa-ark-parent/core-impl/container/src/main/java/com/alipay/sofa/ark/container/service/biz/BizFactoryServiceImpl.java
@@ -38,7 +38,6 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.jar.Attributes;
 
