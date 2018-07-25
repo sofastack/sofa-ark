53d2525fccee4018d7aa9bdf3b11aa20cb532fcf
JoeKerouac <1213812243@qq.com>
2018-07-25 18:28:34 +0800

    add license

--- sofa-ark-parent/core-impl/container/src/main/java/com/alipay/sofa/ark/container/command/PluginCommandProvider.java
+++ sofa-ark-parent/core-impl/container/src/main/java/com/alipay/sofa/ark/container/command/PluginCommandProvider.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alipay.sofa.ark.container.command;
 
 import java.util.Set;
@@ -14,8 +30,8 @@ import com.alipay.sofa.ark.spi.service.plugin.PluginManagerService;
  */
 public class PluginCommandProvider extends AbstractCommandProvider {
     private static String        COMMAND_PRE = "plugin";
-    private PluginManagerService service     = ArkServiceContainerHolder.getContainer()
-        .getService(PluginManagerService.class);
+    private PluginManagerService service     = ArkServiceContainerHolder.getContainer().getService(
+                                                 PluginManagerService.class);
 
     @Override
     protected String getCommandPre() {
