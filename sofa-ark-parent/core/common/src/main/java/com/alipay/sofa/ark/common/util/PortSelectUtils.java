f2d569094ae46fdc7961c05d272b3608cf51fd96
QilongZhang <qilong.zql@alibaba-inc.com>
2018-08-22 16:17:39 +0800

    modify log content

--- sofa-ark-parent/core/common/src/main/java/com/alipay/sofa/ark/common/util/PortSelectUtils.java
+++ sofa-ark-parent/core/common/src/main/java/com/alipay/sofa/ark/common/util/PortSelectUtils.java
@@ -59,7 +59,7 @@ public class PortSelectUtils {
 
     private static boolean available(int port) {
         if (port < MIN_PORT_NUMBER || port > MAX_PORT_NUMBER) {
-            throw new IllegalArgumentException("Invalid start port: " + port);
+            throw new IllegalArgumentException("Invalid port: " + port);
         }
         try (ServerSocket ss = new ServerSocket(port)) {
             ss.setReuseAddress(true);
