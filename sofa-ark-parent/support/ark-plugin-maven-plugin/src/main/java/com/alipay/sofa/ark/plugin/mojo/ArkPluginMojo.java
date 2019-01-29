7d462ed9d53f232a3fea2c50dda04d9701996a9a
QilongZhang <qilong.zql@alibaba-inc.com>
2019-01-29 20:13:47 +0800

    fix #167 (#191)

--- sofa-ark-parent/support/ark-plugin-maven-plugin/src/main/java/com/alipay/sofa/ark/plugin/mojo/ArkPluginMojo.java
+++ sofa-ark-parent/support/ark-plugin-maven-plugin/src/main/java/com/alipay/sofa/ark/plugin/mojo/ArkPluginMojo.java
@@ -42,6 +42,7 @@ import com.alipay.sofa.ark.common.util.StringUtils;
 import com.alipay.sofa.ark.spi.constant.Constants;
 import com.alipay.sofa.ark.tools.ArtifactItem;
 import com.alipay.sofa.ark.tools.JarWriter;
+import com.alipay.sofa.ark.tools.Repackager;
 import org.apache.commons.io.IOUtils;
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.AbstractMojo;
@@ -281,7 +282,9 @@ public class ArkPluginMojo extends AbstractMojo {
                                         Set<Artifact> conflicts) {
         addArtifact(archiver, project.getArtifact(), conflicts.contains(project.getArtifact()));
         for (Artifact artifact : dependencies) {
-            addArtifact(archiver, artifact, conflicts.contains(artifact));
+            if (Repackager.isZip(artifact.getFile())) {
+                addArtifact(archiver, artifact, conflicts.contains(artifact));
+            }
         }
     }
 
