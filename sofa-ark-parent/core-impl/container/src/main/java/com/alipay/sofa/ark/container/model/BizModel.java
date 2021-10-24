2031e2591bec7e2114550d9f16103dab273413f0
尚之 <bingjie.lbj@antgroup.com>
2021-10-25 02:02:30 +0800

    fix bug

--- sofa-ark-parent/core-impl/container/src/main/java/com/alipay/sofa/ark/container/model/BizModel.java
+++ sofa-ark-parent/core-impl/container/src/main/java/com/alipay/sofa/ark/container/model/BizModel.java
@@ -268,14 +268,14 @@ public class BizModel implements Biz {
             resetProperties();
             if (this == ArkClient.getMasterBiz()
                 && "true".equals(System.getProperty(Constants.EMBED_ENABLE))) {
-                //内嵌模式，不再启动master biz
             } else {
                 long start = System.currentTimeMillis();
                 MainMethodRunner mainMethodRunner = new MainMethodRunner(mainClass, args);
                 mainMethodRunner.run();
                 // this can trigger health checker handler
                 eventAdminService.sendEvent(new AfterBizStartupEvent(this));
-                System.out.println("Ark biz [" + getIdentity() + "] started in cost + " + (System.currentTimeMillis() - start) + " ms");
+                System.out.println("Ark biz [" + getIdentity() + "] started in cost  "
+                                   + (System.currentTimeMillis() - start) + " ms");
             }
 
         } catch (Throwable e) {
