aadf2f8503d5bd4b32e5bb42ca5d276e44753ce4
leo james <leojames.googol@gmail.com>
2022-06-14 17:58:01 +0800

    add ark lib for declared (#519)
    * add sofaboot ark plugin
    
    * default classloaderhook only used for provided scope artifact
    
    * remove unused method
    
    * provided check with no library version
    
    * find same class in multi jar from plugin or via classloaderhook.
    
    * fix ut test
    
    * format and optimize performance
    
    * add test
    
    * rename libraries declared in biz moduel
    
    * add test for default classloader hook
    
    * remove unused demo
    
    * get biz module all dep
    
    * support optional maven tree
    
    * update to 2.0.1
    
    * disable test ng common test temporary
    
    * no default ch for test
    
    * update worklfow
    
    * Revert "no default ch for test"
    
    This reverts commit 2748434e9bb9a3c5dd64a440021399cb646b36f8.
    
    * enable compatibility with no declared in biz
    
    * update version
    
    * add demo for multi plugin class isolation
    
    * remove unused code
    
    * add ark dep for biz module declared libs
    
    * fix jar declared checking even no version
    
    * fix test
    
    * add configuration in ark module maven plugin: declaredMode, default is false.
    
    * add thread safe op
    
    * close jarfile
    
    * telnet default setup
    
    Co-authored-by: youji.zzl <youji.zzl@antfin.com>

---
+++ sofa-ark-parent/core/common/src/main/java/com/alipay/sofa/ark/common/util/JarUtils.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alipay.sofa.ark.common.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.jar.Attributes;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
+import java.util.zip.ZipEntry;
+
+import static java.util.jar.Attributes.Name.IMPLEMENTATION_VERSION;
+
+public class JarUtils {
+    private static final String              JAR_POM_PROPERTIES = "pom.properties";
+    private static final String              JAR_VERSION        = "version";
+
+    private static final Map<String, String> jarVersionMap      = new ConcurrentHashMap<>();
+
+    public static String getJarVersion(JarFile jarFile) throws IOException {
+        return jarVersionMap.computeIfAbsent(jarFile.getName(), k -> {
+            try {
+                return doGetJarVersion(jarFile);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    private static String doGetJarVersion(JarFile jarFile) throws IOException {
+        Manifest manifest = jarFile.getManifest();
+        Attributes attributes = manifest.getMainAttributes();
+        String version = attributes.getValue(IMPLEMENTATION_VERSION);
+        if (!StringUtils.isEmpty(version)) {
+            return version;
+        }
+        Enumeration<JarEntry> entries = jarFile.entries();
+        while (entries.hasMoreElements()) {
+            java.util.jar.JarEntry entry = entries.nextElement();
+            if (entry.getName().endsWith(JAR_POM_PROPERTIES)) {
+                try (InputStream is = jarFile.getInputStream(entry)) {
+                    Properties p = new Properties();
+                    p.load(is);
+                    return p.getProperty(JAR_VERSION);
+                }
+            }
+        }
+        return null;
+    }
+}
