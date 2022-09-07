61b288141b79482e9c0c49ff868337029f20cef9
youji.zzl <youji.zzl@antfin.com>
2022-09-07 12:38:12 +0800

    1. update maven version 2. enable mvn dep:tree for set customized local rep

--- sofa-ark-parent/support/ark-maven-plugin/src/main/java/com/alipay/sofa/ark/boot/mojo/RepackageMojo.java
+++ sofa-ark-parent/support/ark-maven-plugin/src/main/java/com/alipay/sofa/ark/boot/mojo/RepackageMojo.java
@@ -16,34 +16,51 @@
  */
 package com.alipay.sofa.ark.boot.mojo;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.*;
-
 import com.alipay.sofa.ark.common.util.ClassUtils;
 import com.alipay.sofa.ark.common.util.ParseUtils;
 import com.alipay.sofa.ark.common.util.StringUtils;
 import com.alipay.sofa.ark.spi.constant.Constants;
 import com.alipay.sofa.ark.tools.ArtifactItem;
+import com.alipay.sofa.ark.tools.Libraries;
+import com.alipay.sofa.ark.tools.Repackager;
 import org.apache.commons.io.FileUtils;
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.artifact.factory.ArtifactFactory;
-import org.apache.maven.artifact.resolver.ArtifactResolver;
+import org.apache.maven.artifact.resolver.ArtifactResolutionRequest;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.model.Dependency;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.descriptor.PluginDescriptor;
-import org.apache.maven.plugins.annotations.*;
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.project.MavenProjectHelper;
+import org.apache.maven.project.ProjectBuildingRequest;
+import org.apache.maven.repository.RepositorySystem;
+import org.apache.maven.shared.invoker.DefaultInvocationRequest;
+import org.apache.maven.shared.invoker.DefaultInvoker;
+import org.apache.maven.shared.invoker.InvocationRequest;
+import org.apache.maven.shared.invoker.InvocationResult;
+import org.apache.maven.shared.invoker.Invoker;
+import org.apache.maven.shared.invoker.MavenInvocationException;
 
-import com.alipay.sofa.ark.tools.Libraries;
-import com.alipay.sofa.ark.tools.Repackager;
-import org.apache.maven.shared.invoker.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
 
 import static com.alipay.sofa.ark.spi.constant.Constants.ARK_CONF_BASE_DIR;
 import static com.alipay.sofa.ark.spi.constant.Constants.EXTENSION_EXCLUDES;
@@ -60,49 +77,49 @@ import static com.alipay.sofa.ark.spi.constant.Constants.EXTENSION_EXCLUDES_GROU
 @Mojo(name = "repackage", defaultPhase = LifecyclePhase.PACKAGE, requiresProject = true, threadSafe = true, requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME, requiresDependencyCollection = ResolutionScope.COMPILE_PLUS_RUNTIME)
 public class RepackageMojo extends AbstractMojo {
 
-    private static final String   BIZ_NAME                   = "com.alipay.sofa.ark.bizName";
+    private static final String    BIZ_NAME                   = "com.alipay.sofa.ark.bizName";
 
     @Parameter(defaultValue = "${project}", readonly = true, required = true)
-    private MavenProject          project;
+    private MavenProject           project;
 
     @Component
-    private MavenProjectHelper    projectHelper;
+    private MavenProjectHelper     projectHelper;
 
     @Component
-    private MavenSession          mavenSession;
+    private MavenSession           mavenSession;
 
     @Component
-    private ArtifactFactory       artifactFactory;
+    private ArtifactFactory        artifactFactory;
 
     @Component
-    private ArtifactResolver      artifactResolver;
+    private RepositorySystem       repositorySystem;
 
     /**
      * Directory containing the generated archive
      * @since 0.1.0
      */
     @Parameter(defaultValue = "${project.build.directory}", required = true)
-    private File                  outputDirectory;
+    private File                   outputDirectory;
 
     @Parameter(defaultValue = "${project.basedir}", required = true)
-    private File                  baseDir;
+    private File                   baseDir;
 
     @Parameter(defaultValue = "", required = false)
-    private String                packExcludesConfig;
+    private String                 packExcludesConfig;
 
     /**
      * Name of the generated archive
      * @since 0.1.0
      */
     @Parameter(defaultValue = "${project.build.finalName}", required = true)
-    private String                finalName;
+    private String                 finalName;
 
     /**
      * Skip the repackage goal.
      * @since 0.1.0
      */
     @Parameter(property = "sofa.ark.repackage.skip", defaultValue = "false")
-    private boolean               skip;
+    private boolean                skip;
 
     /**
      * Classifier to add to the artifact generated. If attach is set 'true', the
@@ -111,27 +128,27 @@ public class RepackageMojo extends AbstractMojo {
      * @since 0.1.0
      */
     @Parameter(defaultValue = "ark-biz", readonly = true)
-    private String                bizClassifier;
+    private String                 bizClassifier;
 
     /**
      *
      */
     @Parameter(defaultValue = "${project.artifactId}")
-    private String                bizName;
+    private String                 bizName;
 
     /**
      * ark biz version
      * @since 0.4.0
      */
     @Parameter(defaultValue = "${project.version}")
-    private String                bizVersion;
+    private String                 bizVersion;
 
     /**
      * ark biz version
      * @since 0.4.0
      */
     @Parameter(defaultValue = "100", property = "sofa.ark.biz.priority")
-    protected Integer             priority;
+    protected Integer              priority;
 
     /**
      * Classifier to add to the executable artifact generated, if needed,
@@ -140,14 +157,14 @@ public class RepackageMojo extends AbstractMojo {
      * @since 0.1.0
      */
     @Parameter(defaultValue = "ark-executable", readonly = true)
-    private String                arkClassifier;
+    private String                 arkClassifier;
 
     /**
      * Attach the module archive to be installed and deployed.
      * @since 0.1.0
      */
     @Parameter(defaultValue = "false")
-    private boolean               attach;
+    private boolean                attach;
 
     /**
      * The name of the main class. If not specified the first compiled class found that
@@ -155,7 +172,7 @@ public class RepackageMojo extends AbstractMojo {
      * @since 0.1.0
      */
     @Parameter
-    private String                mainClass;
+    private String                 mainClass;
 
     /**
      * A list of the libraries that must be unpacked from fat jars in order to run.
@@ -165,93 +182,98 @@ public class RepackageMojo extends AbstractMojo {
      * @since 0.1.0
      */
     @Parameter
-    private List<Dependency>      requiresUnpack;
+    private List<Dependency>       requiresUnpack;
 
     /**
      * The version of SOFAArk, same with plugin version. when developer
      * want develop a application running on the SOFA-Ark. Just configure
      * sofa-ark-maven-plugin.
      */
-    private String                arkVersion;
+    private String                 arkVersion;
+
+    /**
+     * mvn command user properties
+     */
+    private ProjectBuildingRequest projectBuildingRequest;
 
     /**
      * Colon separated groupId, artifactId [and classifier] to exclude (exact match)
      */
     @Parameter(defaultValue = "")
-    private LinkedHashSet<String> excludes                   = new LinkedHashSet<>();
+    private LinkedHashSet<String>  excludes                   = new LinkedHashSet<>();
 
     /**
      * list of groupId names to exclude (exact match).
      */
     @Parameter(defaultValue = "")
-    private LinkedHashSet<String> excludeGroupIds            = new LinkedHashSet<>();
+    private LinkedHashSet<String>  excludeGroupIds            = new LinkedHashSet<>();
 
     /**
      * list of artifact names to exclude (exact match).
      */
     @Parameter(defaultValue = "")
-    private LinkedHashSet<String> excludeArtifactIds         = new LinkedHashSet<>();
+    private LinkedHashSet<String>  excludeArtifactIds         = new LinkedHashSet<>();
 
     /**
      * list of packages denied to be imported
      */
     @Parameter(defaultValue = "")
-    private LinkedHashSet<String> denyImportPackages;
+    private LinkedHashSet<String>  denyImportPackages;
 
     /**
      * list of classes denied to be imported
      */
     @Parameter(defaultValue = "")
-    private LinkedHashSet<String> denyImportClasses;
+    private LinkedHashSet<String>  denyImportClasses;
 
     /**
      * list of resources denied to be imported
      */
     @Parameter(defaultValue = "")
-    private LinkedHashSet<String> denyImportResources;
+    private LinkedHashSet<String>  denyImportResources;
 
     /**
      * list of inject plugin dependencies
      */
     @Parameter(defaultValue = "")
-    private LinkedHashSet<String> injectPluginDependencies   = new LinkedHashSet<>();         ;
+    private LinkedHashSet<String>  injectPluginDependencies   = new LinkedHashSet<>();         ;
 
     /**
      * list of inject plugin export packages
      */
     @Parameter(defaultValue = "")
-    private LinkedHashSet<String> injectPluginExportPackages = new LinkedHashSet<>();         ;
+    private LinkedHashSet<String>  injectPluginExportPackages = new LinkedHashSet<>();         ;
 
     /**
      * whether package provided dependencies into ark
      */
     @Parameter(defaultValue = "false")
-    private boolean               packageProvided;
+    private boolean                packageProvided;
 
     /**
      * whether to skip package ark-executable jar
      */
     @Parameter(defaultValue = "false")
-    private boolean               skipArkExecutable;
+    private boolean                skipArkExecutable;
 
     /**
      * whether to keep ark biz jar after package finish, default value is true
      */
     @Parameter(defaultValue = "true")
-    private boolean               keepArkBizJar;
+    private boolean                keepArkBizJar;
 
     /**
      * web context path when biz is web app. it must start with "/", default value is "/"
      */
     @Parameter(defaultValue = "/", required = true)
-    private String                webContextPath;
+    private String                 webContextPath;
 
     /**
      * the biz jar will record the declared libraries if true,
      * and will filter out only declared libraries when delegate classes and resources to ark-base
      */
     @Parameter(defaultValue = "false")
-    private boolean               declaredMode;
+    private boolean                declaredMode;
 
     @Override
     public void execute() throws MojoExecutionException {
@@ -271,6 +293,8 @@ public class RepackageMojo extends AbstractMojo {
             getLog().debug("skipping repackaging as configuration.");
             return;
         }
+
+        projectBuildingRequest = this.project.getProjectBuildingRequest();
 
         /* version of ark container packaged into fat jar follows the plugin version */
         PluginDescriptor pluginDescriptor = (PluginDescriptor) getPluginContext().get(
@@ -310,8 +334,15 @@ public class RepackageMojo extends AbstractMojo {
         String outputPath = baseDir.getAbsolutePath() + "/deps.log." + System.currentTimeMillis();
         InvocationRequest request = new DefaultInvocationRequest();
         request.setPomFile(new File(baseDir.getAbsolutePath() + "/pom.xml"));
-        request.setGoals(Arrays.asList("dependency:tree", "-DappendOutput=true", "-DoutputFile="
-                                                                                 + outputPath));
+
+        List<String> goals = Arrays.asList("dependency:tree", "-DappendOutput=true",
+            "-DoutputFile=" + outputPath);
+
+        Properties userProperties = projectBuildingRequest.getUserProperties();
+        if (userProperties.contains("maven.repo.local")) {
+            goals.add("-Dmaven.repo.local=" + userProperties.getProperty("maven.repo.local"));
+        }
+        request.setGoals(goals);
         Invoker invoker = new DefaultInvoker();
         try {
             InvocationResult result = invoker.execute(request);
@@ -335,12 +366,18 @@ public class RepackageMojo extends AbstractMojo {
 
     @SuppressWarnings("unchecked")
     private Set<Artifact> getAdditionalArtifact() throws MojoExecutionException {
-        Artifact arkArtifact = artifactFactory.createArtifact(ArkConstants.getGroupId(),
+        Artifact arkArtifact = repositorySystem.createArtifact(ArkConstants.getGroupId(),
             ArkConstants.getArtifactId(), arkVersion, ArkConstants.getScope(),
             ArkConstants.getType());
+
         try {
-            artifactResolver.resolve(arkArtifact, project.getRemoteArtifactRepositories(),
-                mavenSession.getLocalRepository());
+            ArtifactResolutionRequest artifactResolutionRequest = new ArtifactResolutionRequest();
+            artifactResolutionRequest.setArtifact(arkArtifact);
+            artifactResolutionRequest.setLocalRepository(projectBuildingRequest
+                .getLocalRepository());
+            artifactResolutionRequest
+                .setRemoteRepositories(project.getRemoteArtifactRepositories());
+            repositorySystem.resolve(artifactResolutionRequest);
             Set<Artifact> artifacts = new HashSet<>(Collections.singleton(arkArtifact));
             artifacts.addAll(filterExcludeArtifacts(project.getArtifacts()));
             return artifacts;
