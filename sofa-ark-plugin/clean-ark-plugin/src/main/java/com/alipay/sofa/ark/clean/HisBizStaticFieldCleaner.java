88b6f21d8d1966a12c2e06e3c43e4d4552b86e5e
firedemo1 <daiweiv587@gmail.com>
2023-04-09 02:43:26 +0800

    feat:clean static fields implement

---
+++ sofa-ark-plugin/clean-ark-plugin/src/main/java/com/alipay/sofa/ark/clean/HisBizStaticFieldCleaner.java
@@ -0,0 +1,102 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alipay.sofa.ark.clean;
+
+import com.alipay.sofa.ark.api.ArkClient;
+import com.alipay.sofa.ark.common.log.ArkLogger;
+import com.alipay.sofa.ark.common.log.ArkLoggerFactory;
+import com.alipay.sofa.ark.spi.event.biz.AfterBizStopEvent;
+import com.alipay.sofa.ark.spi.model.Biz;
+import com.alipay.sofa.ark.spi.service.PriorityOrdered;
+import com.alipay.sofa.ark.spi.service.event.EventHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author firedemo1
+ */
+@Component
+public class HisBizStaticFieldCleaner implements EventHandler<AfterBizStopEvent> {
+    private final static ArkLogger LOGGER = ArkLoggerFactory.getLogger(HisBizStaticFieldCleaner.class);
+
+    @Override public void handleEvent(AfterBizStopEvent afterBizStopEvent) {
+        Biz biz = afterBizStopEvent.getSource();
+
+        try {
+            if (biz != null && biz != ArkClient.getMasterBiz()) {
+                List<Class<?>> list = findClassesWithStaticFields(biz.getBizClassLoader(), biz.getClassPath()[0].getPath());
+
+                cleanFieldsAndClazz(list);
+            }
+        } catch (Exception e) {
+            LOGGER.error("Clean biz static fields meet exception.errMsg = {}" + e.getMessage());
+        }
+    }
+
+    private void cleanFieldsAndClazz(List<Class<?>> list) {
+        for (Class<?> clazz : list) {
+            try {
+                Field[] fields = clazz.getDeclaredFields();
+                for (Field field : fields) {
+                    if (Modifier.isStatic(field.getModifiers())) {
+                        field.setAccessible(true);
+                        field.set(null, null);
+                    }
+                }
+                clazz = null;
+            } catch (Exception e) {
+                LOGGER.error("Clean static fields meet exception.errMsg = {}" + e.getMessage());
+            }
+        }
+    }
+
+    public List<Class<?>> findClassesWithStaticFields(ClassLoader bizClassLoader, String classPath) {
+        List<Class<?>> classes = new ArrayList<>();
+        File classDir = new File(classPath);
+        File[] classFiles = classDir.listFiles(file -> file.getName().endsWith(".class"));
+        for (File file : classFiles) {
+            String className = file.getName().substring(0, file.getName().lastIndexOf('.'));
+            try {
+                Class<?> cls = bizClassLoader.loadClass(className);
+                if (hasStaticFields(cls)) {
+                    classes.add(cls);
+                }
+            } catch (ClassNotFoundException e) {
+                LOGGER.error("Find classes with static fields meet exception.errMsg = {}" + e.getMessage());
+            }
+        }
+        return classes;
+    }
+
+    private boolean hasStaticFields(Class<?> cls) {
+        for (java.lang.reflect.Field field : cls.getDeclaredFields()) {
+            if (java.lang.reflect.Modifier.isStatic(field.getModifiers())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override public int getPriority() {
+        return PriorityOrdered.HIGHEST_PRECEDENCE;
+    }
+}
