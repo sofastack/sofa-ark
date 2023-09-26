b4a7178972584e5c11fed34351c2aed66359b930
yanhuai.yh <yanhuai.yh@antgroup.com>
2023-09-26 13:10:32 +0800

    feat: Improve unit test to add code coverage for class JarUtils

--- sofa-ark-parent/core-impl/archive/src/test/java/com/alipay/sofa/ark/loader/test/jar/JarUtilsTest.java
+++ sofa-ark-parent/core-impl/archive/src/test/java/com/alipay/sofa/ark/loader/test/jar/JarUtilsTest.java
@@ -54,6 +54,12 @@ public class JarUtilsTest {
         String artifactId = JarUtils.parseArtifactId(clazzURL.getPath());
         Assert.assertEquals("sofa-ark-archive", artifactId);
 
+        URL testClazzURL = this.getClass().getClassLoader()
+                .getResource("com/alipay/sofa/ark/loader/test/jar/JarUtilsTest.class");
+
+        artifactId = JarUtils.parseArtifactId(testClazzURL.getPath());
+        Assert.assertEquals("sofa-ark-archive", artifactId);
+
         URI classPathRoot = this.getClass().getClassLoader().getResource("").toURI();
         String classPath = Paths.get(classPathRoot).getParent().toFile().getAbsolutePath();
         String artifactId1 = JarUtils.parseArtifactId(classPath);
