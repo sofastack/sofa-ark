362f9a80fc248721acb73c03ef065bd6cce13fc0
FanYang <399284508@qq.com>
2023-11-21 17:32:32 +0800

    Ark2.0 support Netty plugin (#653)
    * Ark support Netty plugin
    
    * fix mvn issue
    
    * fix pom issue
    
    ---------
    
    Co-authored-by: Xuhao Wang <1023605039@qq.com>
    Co-authored-by: leo james <leojames.googol@gmail.com>
    Co-authored-by: yuanyuancin <yuancin@163.com>

---
+++ sofa-ark-parent/support/ark-springboot-integration/ark-springboot-starter/src/main/java/com/alipay/sofa/ark/springboot/ArkReactiveAutoConfigure.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alipay.sofa.ark.springboot;
+
+import com.alipay.sofa.ark.springboot.condition.ConditionalOnArkEnabled;
+import com.alipay.sofa.ark.springboot.processor.ArkEventHandlerProcessor;
+import com.alipay.sofa.ark.springboot.processor.ArkServiceInjectProcessor;
+import com.alipay.sofa.ark.springboot.web.ArkNettyReactiveWebServerFactory;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.web.reactive.ReactiveWebServerFactoryAutoConfiguration;
+import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
+import org.springframework.boot.web.embedded.netty.NettyRouteProvider;
+import org.springframework.boot.web.embedded.netty.NettyServerCustomizer;
+import org.springframework.boot.web.reactive.server.ReactiveWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorResourceFactory;
+import reactor.netty.http.server.HttpServer;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Configuration
+@ConditionalOnArkEnabled
+@AutoConfigureBefore(ReactiveWebServerFactoryAutoConfiguration.class)
+public class ArkReactiveAutoConfigure {
+    @Bean
+    public static ArkServiceInjectProcessor serviceInjectProcessor() {
+        return new ArkServiceInjectProcessor();
+    }
+
+    @Bean
+    public static ArkEventHandlerProcessor arkEventHandlerProcessor() {
+        return new ArkEventHandlerProcessor();
+    }
+
+    @Configuration
+    @ConditionalOnMissingBean({ ReactiveWebServerFactory.class })
+    @ConditionalOnClass(value = { HttpServer.class }, name = { "com.alipay.sofa.ark.netty.ArkNettyIdentification" })
+    static class EmbeddedNetty {
+        EmbeddedNetty() {
+        }
+
+        @Bean
+        @ConditionalOnMissingBean
+        ReactorResourceFactory reactorServerResourceFactory() {
+            return new ReactorResourceFactory();
+        }
+
+        @Bean
+        NettyReactiveWebServerFactory nettyReactiveWebServerFactory(ReactorResourceFactory resourceFactory, ObjectProvider<NettyRouteProvider> routes, ObjectProvider<NettyServerCustomizer> serverCustomizers) {
+            NettyReactiveWebServerFactory serverFactory = new ArkNettyReactiveWebServerFactory();
+            serverFactory.setResourceFactory(resourceFactory);
+            routes.orderedStream().forEach((xva$0) -> {
+                serverFactory.addRouteProviders(new NettyRouteProvider[]{xva$0});
+            });
+            serverFactory.getServerCustomizers().addAll((Collection)serverCustomizers.orderedStream().collect(Collectors.toList()));
+            return serverFactory;
+        }
+    }
+}
---
+++ sofa-ark-plugin/netty-ark-plugin/src/main/java/NettyPluginActivator.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import com.alipay.sofa.ark.common.log.ArkLoggerFactory;
+import com.alipay.sofa.ark.netty.EmbeddedServerServiceImpl;
+import com.alipay.sofa.ark.spi.model.PluginContext;
+import com.alipay.sofa.ark.spi.service.PluginActivator;
+import com.alipay.sofa.ark.spi.web.EmbeddedServerService;
+import reactor.netty.http.server.HttpServer;
+
+public class NettyPluginActivator implements PluginActivator {
+
+    private EmbeddedServerService embeddedNettyService = new EmbeddedServerServiceImpl();
+
+    @Override
+    public void start(PluginContext context) {
+        context.publishService(EmbeddedServerService.class, embeddedNettyService);
+    }
+
+    @Override
+    public void stop(PluginContext context) {
+        HttpServer webServer = null;
+        if (embeddedNettyService.getEmbedServer() instanceof HttpServer) {
+            webServer = (HttpServer) embeddedNettyService.getEmbedServer();
+        }
+        if (webServer != null) {
+            try {
+                //webServer.stop();
+            } catch (Exception ex) {
+                ArkLoggerFactory.getDefaultLogger().error("Unable to stop embedded Netty", ex);
+            }
+        }
+    }
+}
