4ade97368303d7acf0a75a9f1839f47efda22fa2
Yan Huai <compasty@163.com>
2023-12-05 20:32:02 +0800

    Export plugin project classes by default (#799)
    * feat: export plugin classes by default.
    
    * feat: export plugin classes by default.
    
    * feat: export plugin classes by default.
    
    * feat: export plugin package by default.
    
    * feat: export plugin package by default.
    
    * default add all packages for plugin
    
    * format
    
    ---------
    
    Co-authored-by: yanhuai.yh <yanhuai.yh@antgroup.com>
    Co-authored-by: leojames <leojames.googol@gmail.com>

--- sofa-ark-parent/core/common/src/test/java/com/alipay/sofa/ark/common/util/ClassUtilsTest.java
+++ sofa-ark-parent/core/common/src/test/java/com/alipay/sofa/ark/common/util/ClassUtilsTest.java
@@ -20,6 +20,10 @@ import com.alipay.sofa.ark.spi.constant.Constants;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author qilong.zql
  * @since 0.3.0
@@ -28,8 +32,36 @@ public class ClassUtilsTest {
 
     @Test
     public void testGetPackageName() {
-        Assert.assertTrue(ClassUtils.getPackageName("a.b.C").equals("a.b"));
-        Assert.assertTrue(ClassUtils.getPackageName("C").equals(Constants.DEFAULT_PACKAGE));
+        Assert.assertEquals("a.b", ClassUtils.getPackageName("a.b.C"));
+        Assert.assertEquals(Constants.DEFAULT_PACKAGE, ClassUtils.getPackageName("C"));
     }
 
+    @Test
+    public void testFindCommonPackage() {
+        Assert.assertEquals(ClassUtils.findCommonPackage(null).size(), 0);
+        List<String> classNames = new ArrayList<>();
+        classNames.add("com.example.project.subpackage1.classE");
+        classNames.add("com.example.project.classA");
+        classNames.add("com.example.project.classB");
+        classNames.add("com.example.project.subpackage.classC");
+        classNames.add("com.example.project.subpackage.classD");
+        Assert.assertEquals(ClassUtils.findCommonPackage(classNames).size(), 3);
+        classNames.add("org.apache.util.ClassF");
+        Assert.assertEquals(ClassUtils.findCommonPackage(classNames).size(), 4);
+    }
+
+    @Test
+    public void testCollectClasses() throws Exception {
+        File dir = new File("target/classes");
+        // fix mvn test fail issues
+        File dir2 = new File(dir.getAbsolutePath());
+        if (!dir2.exists()) {
+            return;
+        }
+
+        List<String> classNames = ClassUtils.collectClasses(dir2);
+        Assert.assertTrue(classNames.contains("com.alipay.sofa.ark.common.util.ClassUtils"));
+        Assert.assertTrue(ClassUtils.findCommonPackage(classNames).contains(
+            "com.alipay.sofa.ark.common.util"));
+    }
 }
