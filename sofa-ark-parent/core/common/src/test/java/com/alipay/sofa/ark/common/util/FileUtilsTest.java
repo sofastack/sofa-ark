491cbfdf84b4f6ab75826c54e1dc32c311eec59f
LiuYu <ly109106@alibaba-inc.com>
2024-01-02 20:00:24 +0800

    update test

--- sofa-ark-parent/core/common/src/test/java/com/alipay/sofa/ark/common/util/FileUtilsTest.java
+++ sofa-ark-parent/core/common/src/test/java/com/alipay/sofa/ark/common/util/FileUtilsTest.java
@@ -17,7 +17,6 @@
 package com.alipay.sofa.ark.common.util;
 
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -25,81 +24,84 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
+import static com.alipay.sofa.ark.common.util.FileUtils.*;
+import static java.lang.System.getProperty;
+import static java.lang.System.setProperty;
+import static org.apache.commons.io.FileUtils.deleteQuietly;
+import static org.apache.commons.io.FileUtils.touch;
+import static org.junit.Assert.*;
+
 /**
  * @author guolei.sgl (guolei.sgl@antfin.com) 2019/7/28 11:24 PM
  * @since
  **/
 public class FileUtilsTest {
 
-    private static final String ORIGIN = System.getProperty("os.name");
+    private static final String ORIGIN = getProperty("os.name");
 
     @Before
     public void before() {
-        System.setProperty("os.name", "windows");
+        setProperty("os.name", "windows");
     }
 
     @After
     public void after() {
-        System.setProperty("os.name", ORIGIN);
+        setProperty("os.name", ORIGIN);
     }
 
     @Test
     public void testGetCompatiblePath() {
-        String winPath = FileUtils.getCompatiblePath("C:\\a\\b\\c");
-        Assert.assertTrue(winPath.contains("/"));
-        String macPath = FileUtils.getCompatiblePath("/a/b/c");
-        Assert.assertTrue(winPath.contains(macPath));
+        String winPath = getCompatiblePath("C:\\a\\b\\c");
+        assertTrue(winPath.contains("/"));
+        String macPath = getCompatiblePath("/a/b/c");
+        assertTrue(winPath.contains(macPath));
     }
 
     @Test
     public void testSHA1Hash() throws IOException {
         URL url = this.getClass().getResource(this.getClass().getSimpleName() + ".class");
-        Assert.assertNotNull(FileUtils.sha1Hash(FileUtils.file(url.getFile())));
+        assertNotNull(sha1Hash(file(url.getFile())));
     }
 
     @Test
     public void testUnzip() throws IOException {
         URL sampleBiz = this.getClass().getClassLoader().getResource("sample-biz.jar");
-        File file = FileUtils.file(sampleBiz.getFile());
-        Assert.assertNotNull(FileUtils.unzip(file, file.getAbsolutePath() + "-unpack"));
+        File file = file(sampleBiz.getFile());
+        assertNotNull(unzip(file, file.getAbsolutePath() + "-unpack"));
     }
 
     @Test
     public void testMkdir() {
-        Assert.assertNull(FileUtils.mkdir(""));
+        assertNull(mkdir(""));
         // test recursive creation
-        File newDir = FileUtils.mkdir("C:\\a\\b\\c");
-        Assert.assertNotNull(newDir);
+        File newDir = mkdir("C:\\a\\b\\c");
+        assertNotNull(newDir);
         // test for exist path
-        Assert.assertNotNull(FileUtils.mkdir("C:\\a\\b\\c"));
+        assertNotNull(mkdir("C:\\a\\b\\c"));
         // del the dir
-        org.apache.commons.io.FileUtils.deleteQuietly(newDir);
+        deleteQuietly(newDir);
     }
 
     @Test
     public void testDecodePath() {
         String path = "C:\\temp dir\\b\\c";
         String encodedPath = "C:\\temp%20dir\\b\\c";
-        Assert.assertEquals(path, FileUtils.decodePath(path));
-        Assert.assertEquals(path, FileUtils.decodePath(encodedPath));
+        assertEquals(path, decodePath(path));
+        assertEquals(path, decodePath(encodedPath));
     }
 
     @Test
     public void testNewFile() throws IOException {
+
         String dir = "C:\\temp dir\\b\\c";
         String encodedPath = "C:\\temp%20dir\\b\\c";
-        FileUtils.mkdir(dir);
-        org.apache.commons.io.FileUtils.touch(new File(dir, "test.txt"));
-        File file = FileUtils.file(encodedPath, "test.txt");
-        Assert.assertNotNull(file);
-        Assert.assertTrue(file.exists());
+        mkdir(dir);
+        touch(new File(dir, "test.txt"));
+        File file = file(encodedPath, "test.txt");
+        assertNotNull(file);
+        assertTrue(file.exists());
 
         file = new File(encodedPath, "test.txt");
-        Assert.assertFalse(file.exists());
-    }
-
-    @Test
-    public void testOtherMethods() {
-
+        assertFalse(file.exists());
     }
 }
