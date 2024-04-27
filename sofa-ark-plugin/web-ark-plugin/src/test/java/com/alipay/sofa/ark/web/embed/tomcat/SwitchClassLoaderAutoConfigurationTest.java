d448e50877083f2181d4d7a9f1e49bbb25359bb2
leojames <leojames.googol@gmail.com>
2024-04-27 11:59:56 +0800

    format

--- sofa-ark-plugin/web-ark-plugin/src/test/java/com/alipay/sofa/ark/web/embed/tomcat/SwitchClassLoaderAutoConfigurationTest.java
+++ sofa-ark-plugin/web-ark-plugin/src/test/java/com/alipay/sofa/ark/web/embed/tomcat/SwitchClassLoaderAutoConfigurationTest.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alipay.sofa.ark.web.embed.tomcat;
 
 import com.alipay.sofa.ark.spi.model.Biz;
@@ -19,8 +35,9 @@ import static org.mockito.Mockito.when;
 
 public class SwitchClassLoaderAutoConfigurationTest {
 
-    private final ApplicationContextRunner contextRunner = new ApplicationContextRunner().withConfiguration(
-            AutoConfigurations.of(SwitchClassLoaderAutoConfiguration.class));
+    private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+                                                             .withConfiguration(AutoConfigurations
+                                                                 .of(SwitchClassLoaderAutoConfiguration.class));
 
     @Before
     public void prepare() {
@@ -43,7 +60,6 @@ public class SwitchClassLoaderAutoConfigurationTest {
             switchClassLoaderFilter.doFilter(new MockHttpServletRequest(), new MockHttpServletResponse(), filterChain);
         });
     }
-
 
     @Test
     public void switchClassLoaderFilter() {
