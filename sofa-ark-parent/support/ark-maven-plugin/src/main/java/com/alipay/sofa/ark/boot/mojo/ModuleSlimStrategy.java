71a1c471ff1a7fae9901c6bf1dacb015b74acd76
2041951416 <134405781+2041951416@users.noreply.github.com>
2024-08-09 16:19:18 +0800

    黑白名单支持All的更新 (#985)
    * 黑白名单支持All配置方式
    
    * 补单测
    
    * 格式化
    
    ---------
    
    Co-authored-by: hxk <h911623516@gmail.com>
    Co-authored-by: leo james <leojames.googol@gmail.com>

--- sofa-ark-parent/support/ark-maven-plugin/src/main/java/com/alipay/sofa/ark/boot/mojo/ModuleSlimStrategy.java
+++ sofa-ark-parent/support/ark-maven-plugin/src/main/java/com/alipay/sofa/ark/boot/mojo/ModuleSlimStrategy.java
@@ -170,6 +170,10 @@ public class ModuleSlimStrategy {
 
     protected Set<Artifact> getArtifactsToFilterByExcludeConfig(Set<Artifact> artifacts) {
         List<ArtifactItem> excludeList = new ArrayList<>();
+        if (config != null
+            && (config.getExcludes().contains("*") || config.getExcludes().contains(".*"))) {
+            return artifacts;
+        }
         for (String exclude : config.getExcludes()) {
             ArtifactItem item = ArtifactItem.parseArtifactItem(exclude);
             excludeList.add(item);
@@ -187,6 +191,10 @@ public class ModuleSlimStrategy {
 
     protected Set<Artifact> getArtifactsToAddByIncludeConfig(Set<Artifact> artifacts) {
         List<ArtifactItem> includeList = new ArrayList<>();
+        if (config != null
+            && (config.getIncludes().contains("*") || config.getIncludes().contains(".*"))) {
+            return artifacts;
+        }
         for (String include : config.getIncludes()) {
             ArtifactItem item = ArtifactItem.parseArtifactItem(include);
             includeList.add(item);
