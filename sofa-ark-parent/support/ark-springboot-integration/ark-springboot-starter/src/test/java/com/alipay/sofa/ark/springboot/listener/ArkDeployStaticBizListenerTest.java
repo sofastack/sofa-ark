d92eb6bfd851a6eefd55ccadfed0fcdf64d6a7a8
Will <65004897+Willhow-Gao@users.noreply.github.com>
2025-01-02 17:36:09 +0800

    Module deployments are deduplicated when static merge deployments(#338) (#1035)
    * Module deployments are deduplicated when static merge deployments(#338)
    
    (cherry picked from commit 41fe0263cf7b4edf6fd915ac3846fedecc702692)

---
+++ sofa-ark-parent/support/ark-springboot-integration/ark-springboot-starter/src/test/java/com/alipay/sofa/ark/springboot/listener/ArkDeployStaticBizListenerTest.java
@@ -0,0 +1,98 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alipay.sofa.ark.springboot.listener;
+
+import com.alipay.sofa.ark.api.ArkConfigs;
+import com.alipay.sofa.ark.support.startup.EmbedSofaArkBootstrap;
+import org.junit.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.ContextStartedEvent;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * @author gaowh
+ * @version 1.0
+ * @time 2024/12/30
+ */
+public class ArkDeployStaticBizListenerTest {
+
+    /**
+     * classloader不匹配的场景
+     */
+    @Test
+    public void testDiffClassLoader() {
+        ArkDeployStaticBizListener listener = new ArkDeployStaticBizListener();
+        ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+        try (MockedStatic<EmbedSofaArkBootstrap> bootstrap = mockStatic(EmbedSofaArkBootstrap.class);
+             MockedStatic<ArkConfigs> arkConfigs = mockStatic(ArkConfigs.class)) {
+            arkConfigs.when(ArkConfigs::isEmbedEnable).thenReturn(true);
+            arkConfigs.when(ArkConfigs::isEmbedStaticBizEnable).thenReturn(true);
+            Thread.currentThread().setContextClassLoader(new ClassLoader() {
+            });
+            listener.onApplicationEvent(new ContextRefreshedEvent(new AnnotationConfigApplicationContext()));
+            bootstrap.verify(Mockito.times(0), EmbedSofaArkBootstrap::deployStaticBizAfterEmbedMasterBizStarted);
+        } finally {
+            Thread.currentThread().setContextClassLoader(contextClassLoader);
+        }
+    }
+
+    /**
+     * applicationEvent 不是 ContextRefreshedEvent 的场景
+     */
+    @Test
+    public void testNonContextRefreshedEvent() {
+        ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+        try (MockedStatic<EmbedSofaArkBootstrap> bootstrap = mockStatic(EmbedSofaArkBootstrap.class);
+             MockedStatic<ArkConfigs> arkConfigs = mockStatic(ArkConfigs.class)) {
+            ClassLoader classLoader = ArkDeployStaticBizListener.class.getClassLoader();
+            Thread.currentThread().setContextClassLoader(classLoader);
+            arkConfigs.when(ArkConfigs::isEmbedEnable).thenReturn(true);
+            arkConfigs.when(ArkConfigs::isEmbedStaticBizEnable).thenReturn(true);
+            ArkDeployStaticBizListener listener = new ArkDeployStaticBizListener();
+            listener.onApplicationEvent(new ContextStartedEvent(new AnnotationConfigApplicationContext()));
+            bootstrap.verify(Mockito.times(0), EmbedSofaArkBootstrap::deployStaticBizAfterEmbedMasterBizStarted);
+        } finally {
+            Thread.currentThread().setContextClassLoader(contextClassLoader);
+        }
+    }
+
+    /**
+     * 事件重复发送的场景
+     */
+    @Test
+    public void testDeployed() {
+        ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+        try (MockedStatic<EmbedSofaArkBootstrap> bootstrap = mockStatic(EmbedSofaArkBootstrap.class);
+             MockedStatic<ArkConfigs> arkConfigs = mockStatic(ArkConfigs.class)) {
+            ClassLoader classLoader = ArkDeployStaticBizListener.class.getClassLoader();
+            Thread.currentThread().setContextClassLoader(classLoader);
+            arkConfigs.when(ArkConfigs::isEmbedEnable).thenReturn(true);
+            arkConfigs.when(ArkConfigs::isEmbedStaticBizEnable).thenReturn(true);
+            ArkDeployStaticBizListener listener = new ArkDeployStaticBizListener();
+            listener.onApplicationEvent(new ContextRefreshedEvent(new AnnotationConfigApplicationContext()));
+            // 容器刷新事件已经发送过，重复发送不会重复部署
+            listener.onApplicationEvent(new ContextRefreshedEvent(new AnnotationConfigApplicationContext()));
+            bootstrap.verify(Mockito.times(1), EmbedSofaArkBootstrap::deployStaticBizAfterEmbedMasterBizStarted);
+        } finally {
+            Thread.currentThread().setContextClassLoader(contextClassLoader);
+        }
+    }
+}
